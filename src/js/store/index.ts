c4c3561f375b122ea12496bc55d2692d66215b07
Rydal <i.am.alex2k@gmail.com>
2020-12-30 01:20:23 +0300

    fixed invisible header

--- src/js/store/index.ts
+++ src/js/store/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import common from './common'
+import $ from 'jquery'
 
 Vue.use(Vuex)
 
@@ -14,6 +15,7 @@ export default new Vuex.Store({
       addEventListener('resize', () => {
         this.commit('common/setWindowWidth')
       })
+      this.commit('common/setWindowWidth')
     }
   },
   modules: {
